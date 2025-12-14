035a9f78c9f844948b6fa9c7aa7b0130306adead
karnVen <onemonth06@gmail.com>
2025-12-14 16:53:06 +0530

    5th page learings , just the rules of jsx

--- 02_react_app/src/Greeting.jsx
+++ 02_react_app/src/Greeting.jsx
@@ -1,4 +1,37 @@
-function Greeting(){
+export function Greeting(){
     return <h1>&quot;I swear by my pretty floral bonnet, I will end you.&quot;</h1>
 }
-export default Greeting;
+export function Rule() {
+  // Could replace <></> with <div></div>
+  return (
+    <>
+      <h1>Example h1</h1>
+      <h2>Example h2</h2>
+    </>
+  );
+}
+export function Circle() {
+  return (
+  <div className="container">
+    <svg>
+      <circle cx="55" cy="45" r="20" stroke="green" strokeWidth="8" />
+    </svg>
+  </div>
+  );
+}
+export function LiveError(){
+    return ( 
+   <div>
+  <h1>Test title</h1>
+  <svg>
+    <circle cx="25" cy="75" r="20" stroke="green" strokeWidth="2" />
+  </svg>
+  <form>
+    <input type="text" />
+  </form>
+</div>
+
+
+)
+}
+
